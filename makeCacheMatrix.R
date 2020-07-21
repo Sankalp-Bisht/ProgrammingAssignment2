01ae7497d447f0118a7389a40e049d9e52d4bb23
Sankalp Singh Bisht <46498101+Sankalp-Bisht@users.noreply.github.com>
2020-07-22 00:27:46 +0530

    Add files via upload

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,3 +1,10 @@
+#The first function, makeVector creates a special "vector", which is really a list containing a function to
+
+#set the value of the vector
+#get the value of the vector
+#set the value of the mean
+#get the value of the mean
+
 makeCacheMatrix <- function(mat = matrix()){ ##First Function of Assignment
   
   m=NULL
@@ -23,5 +30,30 @@ makeCacheMatrix <- function(mat = matrix()){ ##First Function of Assignment
     list(set = set, get = get,
          setInverse = setInverse,
          getInverse = getInverse)
+}
+
+
+
+#cacheSolve: This function computes the inverse of the special "matrix" returned
+#by makeCacheMatrix above. If the inverse has already been calculated (and the 
+#matrix has not changed), then the cachesolve should retrieve the inverse from 
+#the cache.
+
+
+cacheSolve <- function(x, ...){ ##Second Function of Assignment
+  m <- x$getInverse()
+  
+  if(!is.null(m)) { ##Checking for already present data
+    
+    message("getting cached data")
+    return(m)
+    
   }
+  
+  data <- x$get()   ##Setting if cache data not found
+  m <- Inverse(data, ...)
+  x$setInverse(m)
+  m
+}
+
   
